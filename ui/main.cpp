ab65910656654c18fd19c9e12f40dd0f8a98cdf5
Karim Naaji <karim.naaji@gmail.com>
2016-02-18 22:39:59 -0500

    Drag and drop ui mode

--- ui/main.cpp
+++ ui/main.cpp
@@ -1,7 +1,16 @@
 #include "oglw.h"
+#define VOXELIZER_IMPLEMENTATION
+#include "../voxelizer.h"
 
 using namespace OGLW;
 using namespace std;
+
+struct MeshVertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+};
+
+unique_ptr<Mesh<MeshVertex>> m = nullptr;
 
 class Viewer : public App {
     public:
@@ -12,47 +21,114 @@ class Viewer : public App {
     private:
         unique_ptr<Shader> m_shader;
         unique_ptr<Texture> m_texture;
-        unique_ptr<RawMesh> m_mesh;
         float m_xrot = 0.f, m_yrot = 0.f;
 };
 OGLWMain(Viewer);
 
 void dropCallback(GLFWwindow* window, int count, const char** paths) {
     INFO("%s\n", paths[0]);
+
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    tinyobj::LoadObj(shapes, materials, paths[0]);
+
+    for (size_t i = 0; i < shapes.size(); i++) {
+        if (i == 1) { break; } // First shape for now
+        vx_mesh_t* mesh;
+        vx_mesh_t* result;
+
+        mesh = vx_mesh_alloc(shapes[i].mesh.positions.size(), shapes[i].mesh.indices.size());
+
+        for (size_t f = 0; f < shapes[i].mesh.indices.size(); f++) {
+            mesh->indices[f] = shapes[i].mesh.indices[f];
+        }
+
+        for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) {
+            mesh->vertices[v].x = shapes[i].mesh.positions[3*v+0];
+            mesh->vertices[v].y = shapes[i].mesh.positions[3*v+1];
+            mesh->vertices[v].z = shapes[i].mesh.positions[3*v+2];
+        }
+
+        result = vx_voxelize(mesh, 0.03, 0.03, 0.03, 0.02);
+
+        INFO("Number of vertices: %ld\n", result->nvertices);
+        INFO("Number of indices: %ld\n", result->nindices);
+
+        static auto layout = std::shared_ptr<VertexLayout>(new VertexLayout({
+            {"position", 3, GL_FLOAT, false, 0, AttributeLocation::position},
+            {"normal", 3, GL_FLOAT, false, 0, AttributeLocation::normal},
+        }));
+
+        m = std::make_unique<Mesh<MeshVertex>>(layout, GL_TRIANGLES);
+
+        std::vector<MeshVertex> vertices;
+        std::vector<int> indices(result->indices, result->indices + result->nindices);
+
+        vertices.reserve(result->nvertices);
+
+        for (size_t i = 0; i < result->nindices; ++i) {
+            vertices.push_back({
+                {result->vertices[result->indices[i]].x,
+                 result->vertices[result->indices[i]].y,
+                 result->vertices[result->indices[i]].z},
+                {result->normals[result->normalindices[i]].x,
+                 result->normals[result->normalindices[i]].y,
+                 result->normals[result->normalindices[i]].z},
+            });
+        }
+
+        //m->addVertices(std::move(vertices), std::move(indices));
+        m->addVertices(std::move(vertices), {});
+
+        vx_mesh_free(result);
+        vx_mesh_free(mesh);
+    }
 }
 
 void Viewer::init() {
-    // Toggle gui mode by default
+
     App::guiMode = true;
+
     glClearColor(1.0, 1.0, 1.0, 1.0);
-    //m_camera.setPosition({0.0, 0.0, 10.0});
-    //m_camera.setNear(5.0);
-    //m_camera.setFar(15.0);
-    //m_shader = make_unique<Shader>("default.glsl");
-    //m_mesh = loadOBJ(s_tileOBJ);
-    //m_texture = make_unique<Texture>(s_atlasAO);
-    //RenderState::depthWrite(GL_TRUE);
-    //RenderState::depthTest(GL_TRUE);
-    //RenderState::culling(GL_TRUE);
-    //RenderState::cullFace(GL_BACK);
+
+    m_camera.setPosition({0.0, -0.5, 10.0});
+    m_camera.setNear(5.0);
+    m_camera.setFar(15.0);
+
+    m_shader = make_unique<Shader>("default.glsl");
 
     glfwSetDropCallback(m_window, dropCallback);
+
 }
 
 void Viewer::update(float _dt) {
-    //m_xrot += m_cursorX;
-    //m_yrot += m_cursorY;
+
+    if (!App::guiMode) {
+        m_xrot += m_cursorX;
+        m_yrot += m_cursorY;
+    }
+
 }
 
 void Viewer::render(float _dt) {
-    //glm::mat4 model, view = m_camera.getViewMatrix();
-    //model = glm::rotate(model, m_xrot * 1e-2f, glm::vec3(0.0, 1.0, 0.0));
-    //model = glm::rotate(model, m_yrot * 1e-2f, glm::vec3(1.0, 0.0, 0.0));
-    //glm::mat4 mvp = m_camera.getProjectionMatrix() * view * model;
-    //m_texture->bind(0);
-    //m_shader->setUniform("ao", 0);
-    //m_shader->setUniform("mvp", mvp);
-    //m_mesh->draw(*m_shader);
+
+    if (m) {
+        RenderState::depthWrite(GL_TRUE);
+        RenderState::depthTest(GL_TRUE);
+        RenderState::culling(GL_TRUE);
+        RenderState::cullFace(GL_BACK);
+
+        glm::mat4 model, view = m_camera.getViewMatrix();
+
+        model = glm::rotate(model, m_xrot * 1e-2f, glm::vec3(0.0, 1.0, 0.0));
+        model = glm::rotate(model, m_yrot * 1e-2f, glm::vec3(1.0, 0.0, 0.0));
+
+        glm::mat4 mvp = m_camera.getProjectionMatrix() * view * model;
+
+        m_shader->setUniform("mvp", mvp);
+
+        m->draw(*m_shader);
+    }
 
     oglwImGuiBegin();
 
@@ -62,10 +138,11 @@ void Viewer::render(float _dt) {
         ImGui::Text("Hello, world!");
         ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
         ImGui::ColorEdit3("clear color", (float*)&clear_color);
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+            1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     }
 
-
     oglwImGuiFlush();
+
 }
 
--- ui/resources/default.glsl
+++ ui/resources/default.glsl
@@ -2,31 +2,27 @@
 #version 330
 in vec3 position;
 in vec3 normal;
-in vec2 uv;
 
 uniform mat4 mvp;
 
-out vec2 fuv;
 out vec3 fnormal;
 
 void main() {
-    // flip uv on y axis
-    fuv = vec2(uv.x, 1.0 - uv.y);
+    fnormal = normal;
     gl_Position = mvp * vec4(position, 1.0);
 }
 #pragma end:vertex
 
 #pragma begin:fragment
 #version 330
-uniform sampler2D ao;
 
-in vec2 fuv;
 in vec3 fnormal;
 
 out vec4 outColour;
 
 void main(void) {
-    outColour = vec4(texture(ao, fuv).rgb, 1.0);
-    //outColour = vec4(fuv, 0.0, 1.0);
+    outColour = vec4(fnormal, 1.0);
+    //outColour = vec4(vec3(0.5), 1.0);
 }
+
 #pragma end:fragment
