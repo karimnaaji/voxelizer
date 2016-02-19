fb6377d1e561ec4f961db28f47992b08747c2723
Karim Naaji <karim.naaji@gmail.com>
2016-02-19 14:43:58 -0500

    Licence date

--- voxelizer.h
+++ voxelizer.h
@@ -2,7 +2,7 @@
 // LICENCE:
 //  The MIT License (MIT)
 //
-//  Copyright (c) 2014 Karim Naaji, karim.naaji@gmail.com
+//  Copyright (c) 2016 Karim Naaji, karim.naaji@gmail.com
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
